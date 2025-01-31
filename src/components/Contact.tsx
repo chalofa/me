b1c87d6de818c597c45d0e1b520266e9bf26e141
Chalo Fernandez <chalofa@gmail.com>
2025-01-31 12:38:21 -0600

    Contact: schedule call button

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 import '../assets/styles/Contact.scss';
 
 function Contact() {
@@ -10,9 +12,13 @@ function Contact() {
             <a href="https://calendly.com/chalofa/chat" title="Calendly link" target="_blank" rel="noreferrer">Let's meet!</a>
           </h1>
           <p>
-            If you have an exciting opportunity/project where we can collaborate, lets have a
-            quick <a href="https://calendly.com/chalofa/chat" target="_blank" rel="noreferrer">intro call</a>.
+            If you have an exciting opportunity/project where we can collaborate, lets have
+            a <a href="https://calendly.com/chalofa/chat" target="_blank" rel="noreferrer">quick chat</a>.
           </p>
+
+          <Button href="https://calendly.com/chalofa/chat" variant="contained" endIcon={<SendIcon />} target="_blank">
+            Schedule call
+          </Button>
         </div>
       </div>
     </div>
